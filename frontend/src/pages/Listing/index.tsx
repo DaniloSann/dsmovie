8dee74986f2f5058402c507c3f70e77b13802cb7
Danilo Santana <danilosan262020@gmail.com>
2022-01-26 20:21:50 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,14 +1,22 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-  axios.get('${BASE_URL}/movies')
-    .then(response => {
-      console.log(response.data)
-    });
+  const [pageNumver, setPageNumber] = useState(0);
 
+  useEffect() => {
+    axios.get('${BASE_URL}/movies')
+    .then(response => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+    });
+  }, [];
+
+  
   return (
     <>
       <Pagination />
